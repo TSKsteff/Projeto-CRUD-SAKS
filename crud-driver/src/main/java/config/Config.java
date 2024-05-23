adbb45712e5714bb1e0bc3c1bc3450a7b0a62f81
Steff Kerry <sktoussaint@stefanini.com>
2024-05-22 22:32:56 -0300

    Establecendo conecao com o banco de dados

---
+++ crud-driver/src/main/java/config/Config.java
@@ -0,0 +1,46 @@
+package config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Config {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection(){
+        if(connection == null){
+            try {
+                Properties properties = loadProperties();
+                String url = properties.getProperty("dburl");
+                connection = DriverManager.getConnection(url, properties);
+            }catch (SQLException e){
+                throw new DbExeption(e.getMessage());
+            }
+        }
+        return connection;
+    }
+
+    private static Properties loadProperties() {
+        try(FileInputStream fileInputStream = new FileInputStream("db.properties")){
+            Properties properties = new Properties();
+            properties.load(fileInputStream);
+            return properties;
+        }catch (IOException e){
+            throw new DbExeption(e.getMessage());
+        }
+    }
+
+    public static void closeConnection(){
+        if(connection != null){
+            try{
+                connection.close();
+            }catch (SQLException e){
+                throw new DbExeption(e.getMessage());
+            }
+        }
+    }
+}
